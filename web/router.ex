cf4317a27b7897bf3625471c1ec73e7ecb4e5ba6
loongmxbt <loongmxbt@gmail.com>
2016-08-17 22:52:13 +0800

    fix login:true

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,7 @@ defmodule Confer.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug Coherence.Authentication.Session, protected: true
+    plug Coherence.Authentication.Session, login: true
   end
 
   pipeline :auth_admin do
