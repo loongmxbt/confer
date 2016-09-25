fea424e9d2463550e3c7f8003bdd7a3c715e1c6e
loongmxbt <loongmxbt@gmail.com>
2016-09-25 22:15:15 +0800

    prod port -> paper url

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :confer, Confer.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "www.thermophysics.cn", port: 8080],
+  url: [host: "www.thermophysics.cn", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
