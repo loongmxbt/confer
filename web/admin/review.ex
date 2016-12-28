ac51f2524e7e3f50d028b597f572afb381474769
loongmxbt <loongmxbt@gmail.com>
2016-12-28 21:23:23 +0800

    review only prof

--- web/admin/review.ex
+++ web/admin/review.ex
@@ -1,7 +1,21 @@
 defmodule Confer.ExAdmin.Review do
   use ExAdmin.Register
+  alias Confer.{Repo, Paper, User, Status}
 
   register_resource Confer.Review do
     menu priority: 32, label: "评阅"
+
+    form review do
+      inputs do
+        input review, :paper, collection: Repo.all(Paper)
+        input review, :user, collection: Repo.all(from u in User, where: u.role_id == 2)
+        input review, :status, collection: Repo.all(Status)
+        input review, :content
+      end
+    end
+
   end
+
+  # form profs
+
 end
